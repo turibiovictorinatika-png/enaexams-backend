bee02e0c1e7273778d957ff59a327d96c13a275d
turibiovictorinatika <turibiovictorinatika@gmail.com>
2026-05-03 01:15:11 +0100

    Fix CORS open

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const app = express();
 
 // ─── Middleware ───
 app.use(cors({
-  origin: 'https://enaexams.vercel.app',
+  origin: function(origin, callback) {
+    callback(null, true); // accepte toutes les origines
+  },
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
